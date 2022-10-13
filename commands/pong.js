f41fc1b5d0289265b753cd38efc67db6b75960fc
Leandro Battocchio <leandrobattocchio@hotmail.com>
2022-10-13 18:04:46 -0300

    command mate

--- commands/pong.js
+++ commands/pong.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setName('mate')
+    .setDescription('Pedis un mate'),
   async execute (interaction) {
-    await interaction.reply('Pong!')
+    await interaction.reply('Toma un matecito pa')
   }
 }
