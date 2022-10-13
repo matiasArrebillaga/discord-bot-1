19d57e4839c0cc272613bcf6c4319089b13263bb
Leandro Battocchio <leandrobattocchio@hotmail.com>
2022-10-13 19:50:26 -0300

    tenor

--- commands/mate.js
+++ commands/mate.js
@@ -1,11 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js')
-const mate = 'https://tenor.com/view/tea-mate-gif-12753621'
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('mate')
     .setDescription('Pedis un mate'),
   async execute (interaction) {
-    await interaction.reply(`Toma un matecito pa\n${mate}`)
+    await interaction.reply('Tomate un mate pa')
   }
 }
