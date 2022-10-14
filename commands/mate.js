e6b87f44930e2ccb08e0c6df61a86ef617ecc4d8
Leandro Battocchio <leandrobattocchio@hotmail.com>
2022-10-13 21:45:27 -0300

    mate foto

--- commands/mate.js
+++ commands/mate.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName('mate')
     .setDescription('Pedis un mate'),
   async execute (interaction) {
-    await interaction.reply('Toma un matecito pa')
+    await interaction.reply('https://tenor.com/view/tea-mate-gif-12753621\nToma un matecito pa')
   }
 }
